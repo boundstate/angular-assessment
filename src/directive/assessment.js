b555f25df8860e283843b77cb41f2f4d21909a83
Mike Peters <mpeters@boundstatesoftware.com>
2020-03-22 07:41:11 -0700

    feat: specify assessment offset

--- src/directive/assessment.js
+++ src/directive/assessment.js
@@ -3,10 +3,11 @@ angular.module('boundstate.assessment')
 .directive('assessment', function(assessment) {
   return {
     restrict: 'AE',
-    scope: {},
+    scope: {
+      offset: '='
+    },
     link: function(scope, el, attrs) {
       scope.questions = assessment.getQuestions();
-      scope.offset = el[0].getBoundingClientRect().top;
     },
     templateUrl: 'directive/assessment.tpl.html',
     replace: true
